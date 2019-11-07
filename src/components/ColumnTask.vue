4f0769d1e32d9102f67d548ede4f9c05ddd1cadd
RedCAT <akaineko18@gmail.com>
2019-11-07 13:07:30 +1300

    refactoring and extracting drag and drop

---
+++ src/components/ColumnTask.vue
@@ -0,0 +1,57 @@
+<template>
+  <AppDrop
+    @drop="moveTaskOrColumn"
+  >
+    <AppDrag
+      class="task"
+      :transferData="{
+        type: 'task',
+        fromColumnIndex: columnIndex,
+        fromTaskIndex: taskIndex
+      }"
+      @click="goToTask(task)"
+    >
+      <span class="w-full flex-no-shrink font-bold">
+        {{ task.name }}
+      </span>
+      <p v-if="task.description" class="w-full flex-no-shrink mt-1 text-sm">
+        {{ task.description }}
+      </p>
+    </AppDrag>
+  </AppDrop>
+</template>
+
+<script>
+import AppDrag from './AppDrag';
+import AppDrop from './AppDrop';
+import movingTasksAndColumnsMixin from '../mixins/movingTasksAndColumnsMixin.js';
+
+export default {
+  props: {
+    task: {
+      type: Object,
+      required: true
+    },
+    taskIndex: {
+      type: Number,
+      required: true
+    }
+  },
+  components: {
+    AppDrag,
+    AppDrop
+  },
+  mixins: [movingTasksAndColumnsMixin],
+  methods: {
+    goToTask (task) {
+      this.$router.push({ name: 'task', params: { id: task.id } })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.task {
+  @apply flex items-center flex-wrap shadow mb-2 py-2 px-2 rounded bg-white text-grey-darkest no-underline;
+}
+</style>
